56405941d01cad95ed18843fe5e67c52d4aba89e
tj <tj@DESKTOP-A4A3OS0>
2024-11-06 10:40:40 +0900

    user

---
+++ atm_1/src/atm/User.java
@@ -0,0 +1,20 @@
+package atm;
+
+import java.util.ArrayList;
+
+public class User {
+	private int code;
+	private String name;
+	private ArrayList<Account> accounts;
+	
+	public User(int code, String name) {
+		this.name=name;
+		this.code=code;
+	}
+	public void addAcount(Account account) {
+		accounts.add(account);
+	}
+	public void removeAcount(Account account) {
+		accounts.remove(account);
+	}
+}
