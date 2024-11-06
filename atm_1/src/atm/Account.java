d5c2caaff81345baa8be01f19de5927c04b7bcb7
tj <staralbum2@gmail.com>
2024-11-06 13:54:32 +0900

    cnt추가

--- atm_1/src/atm/Account.java
+++ atm_1/src/atm/Account.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 public class Account {
 	private int num;
 	private int money;
+	private int cnt;
 	
 	Account (int num){
-		this.num = num;	
+		this.num = num;
+		this.cnt++;
 	}
 	public void setMoney(int money) {
 		this.money = money;
@@ -18,6 +20,9 @@ public class Account {
 	
 	public int getNum() {
 		return this.num;
+	}
+	public int getCnt() {
+		return this.cnt;
 	}
 	
 }
