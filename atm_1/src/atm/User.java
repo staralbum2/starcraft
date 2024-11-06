da5f930a7f8b777caec2e056c65b694db651efa1
tj <staralbum2@gmail.com>
2024-11-06 12:31:48 +0900

    user 수정

--- atm_1/src/atm/User.java
+++ atm_1/src/atm/User.java
@@ -5,11 +5,15 @@ import java.util.ArrayList;
 public class User {
 	private int code;
 	private String name;
+	private String id;
+	private String passWord;
 	private ArrayList<Account> accounts;
 	
-	public User(int code, String name) {
+	public User(int code, String name,String id,String passWord) {
 		this.name=name;
 		this.code=code;
+		this.id=id;
+		this.passWord = passWord;
 	}
 	public void addAcount(Account account) {
 		accounts.add(account);
@@ -17,4 +21,4 @@ public class User {
 	public void removeAcount(Account account) {
 		accounts.remove(account);
 	}
-}1
+}
