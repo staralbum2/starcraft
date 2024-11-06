9a0e3d4938cd16ddf69d0709bd25914b53dc013f
tj <tj@DESKTOP-A4A3OS0>
2024-11-06 11:28:26 +0900

    set final int

--- atm_1/src/atm/AtmSystem.java
+++ atm_1/src/atm/AtmSystem.java
@@ -1,12 +1,33 @@
 package atm;
 
 public class AtmSystem {
+	private final int JOIN = 1;
+	private final int LOG_IN = 2;
+	private final int ACCOUNT = 1;
+	private final int ADD = 1;
+	private final int REMOVE = 2;
+	private final int DEPOSIT = 2;
+	private final int WITHDRAWAL = 3;
+	private final int TRANSFER = 4;
+	private final int CHECK = 5;
+	private final int SAVE = 6;
+	private final int LOAD = 7;
+	private final int EXIT = 0;
+	
+	
+	
+	
 	public void run() {
 		// 회원탈퇴,가입
 		// 계좌 신청 철회(1인 3개맥스)
 		// 로그인
 		// +뱅킹기능 입출금조회이체계좌신청철회
 		//파일 저장 로드
+		while(true) {
+			int sel = input("메뉴 선택");
+			
+			
+		}
 		
 	}
 }
