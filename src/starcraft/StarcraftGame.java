4798f6b15f93baa5b2f40a2b7cb91b3e2e10cc9c
unknown <staralbum2@gmail.com>
2024-11-08 15:29:45 +0900

    update unit

--- src/starcraft/StarcraftGame.java
+++ src/starcraft/StarcraftGame.java
@@ -1,5 +1,10 @@
 package starcraft;
 
 public class StarcraftGame {
-
+	private static StarcraftGame starcraftGame= new StarcraftGame();
+	private StarcraftGame() {
+	}
+	static StarcraftGame getInstance() {
+		return starcraftGame;
+	}
 }
--- src/starcraft/Unit.java
+++ src/starcraft/Unit.java
@@ -1,5 +1,14 @@
 package starcraft;
 
-public class Unit {
-
+abstract public class Unit {
+	private int hp;
+	private final int MAX_HP;
+	private int power;
+	
+	public Unit(int hp, int power){
+		this.hp = hp;
+		MAX_HP = hp;
+		this.power = power;
+	}
+	
 }
